e7e613b155fe478746e4a4b9a93ff79331d96dc4
jngo2 <james.ngo@toronto.ca>
2022-04-30 17:19:55 -0400

    Expand svg loader

--- webpack.config.test.js
+++ webpack.config.test.js
@@ -90,7 +90,14 @@ module.exports = (env, { mode }) => {
 					}
 				},
 				{
-					test: /\.svg$/,
+					test: /\.svg$/i,
+					type: 'asset',
+					resourceQuery: /url/ // *.svg?url
+				},
+				{
+					test: /\.svg$/i,
+					issuer: /\.[jt]sx?$/,
+					resourceQuery: { not: [/url/] }, // exclude react component if *.svg?url
 					use: ['@svgr/webpack']
 				},
 				{
