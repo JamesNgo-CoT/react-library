a3f51ce4e14831437707a9f829bf66ca9a59bde0
jngo2 <james.ngo@toronto.ca>
2022-05-01 14:14:32 -0400

    Remove bootstrap imports

--- test/index.js
+++ test/index.js
@@ -1,10 +1,3 @@
-/* eslint-disable no-unused-vars */
-import 'bootstrap/dist/css/bootstrap.min.css';
-import $ from 'jquery';
-import Popper from 'popper.js';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-/* eslint-enable no-unused-vars */
-
 import React from 'react';
 import ReactDomClient from 'react-dom/client';
 
